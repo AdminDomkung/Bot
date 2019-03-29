538c8f6a9e3d9082cd7b8e80c02f9e7550b5a987
AdminDomkung <34836897+AdminDomkung@users.noreply.github.com>
2019-03-29 18:23:47 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,9 +1,6 @@
 <?php
-echo "Hello BOT test";
-require "vendor/autoload.php";
-require_once('vendor/linecorp/line-bot-sdk/line-bot-sdk-tiny/LINEBotTiny.php');
-    $accessToken = "HBNoIoQBeQHyRxCeCGRjYDad526mABEto9JFoh8C8cnyzLzBzduVEEYicXKgXNQIEuyyk6Z15W6jTtZ7VunhobqK0zrKg97CgKuFqtehfedFGPek35rU7VDa4/2gMe8t/zbH51Z5speNkzGGbyfkEQdB04t89/1O/w1cDnyilFU=";
-   $content = file_get_contents('php://input');
+// Get POST body content
+$content = file_get_contents('php://input');
 // Parse JSON
 $events = json_decode($content, true);
 // Validate parsed JSON data
@@ -23,23 +20,23 @@ $messages = [
 ];
 // Make a POST Request to Messaging API to reply to sender
 $url = 'https://api.line.me/v2/bot/message/reply';
-    $data = [
-    'replyToken' => $replyToken,
-    'messages' => [$messages],
-    ];
-        $post = json_encode($data);
-        $headers = array('Content-Type: application/json', 'Authorization: Bearer ' . $access_token);
-        $ch = curl_init($url);
-        curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
-        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-        curl_setopt($ch, CURLOPT_POSTFIELDS, $post);
-        curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
-        curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
-        $result = curl_exec($ch);
-        curl_close($ch);
-        echo $result . "\r\n";
-        }
-    }
+$data = [
+'replyToken' => $replyToken,
+'messages' => [$messages],
+];
+$post = json_encode($data);
+$headers = array('Content-Type: application/json', 'Authorization: Bearer ' . $access_token);
+$ch = curl_init($url);
+curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+curl_setopt($ch, CURLOPT_POSTFIELDS, $post);
+curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
+$result = curl_exec($ch);
+curl_close($ch);
+echo $result . "\r\n";
+}
+}
 }
 echo "OK";
 ?>
